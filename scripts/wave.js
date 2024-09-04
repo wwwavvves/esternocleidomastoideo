166fad678f3c7de03a8af13522bd973e681127d6
Diana <wwwavvves@gmail.com>
2024-09-04 19:48:24 +0100

    File structure and homepage

---
+++ scripts/wave.js
@@ -0,0 +1,31 @@
+// Make waves
+let xs = [];
+for (var i = 0; i <= 500; i++) {
+  xs.push(i);
+}
+
+let t = 0;
+
+function animate() {
+  let points = xs.map((x) => {
+    let y = 200 + 15 * Math.sin((x + t) / 15);
+
+    return [x, y];
+  });
+
+  let wave =
+    "M" +
+    points
+      .map((p) => {
+        return p[0] + "," + p[1];
+      })
+      .join(" L");
+
+  document.querySelector(".wave").setAttribute("d", wave);
+
+  t += 0.5;
+
+  requestAnimationFrame(animate);
+}
+
+animate();
