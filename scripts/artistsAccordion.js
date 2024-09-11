837e81bd60af9e3c573fb1059c1b55b4c710270b
Diana <wwwavvves@gmail.com>
2024-09-11 15:57:08 +0100

    Fixed artists accordion

---
+++ scripts/artistsAccordion.js
@@ -0,0 +1,62 @@
+// Fetch the JSON data
+fetch("../data/data.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.json(); // Parse the JSON from the response
+  })
+  .then((data) => {
+    // Handle the data here
+    displayArtists(data.artists);
+  })
+  .catch((error) => {
+    console.error("There has been a problem with your fetch operation:", error);
+  });
+
+function displayArtists(artists) {
+  const accordion = document.querySelector(".accordion");
+  artists
+    .slice()
+    .reverse()
+    .forEach((element) => {
+      let elementIdPadded = element.id.toString().padStart(3, "0");
+      let html = `
+        <div class="acc-item">
+              <button class="accordion-btn">
+                <div class="artist-name"><span class="artist-id">${elementIdPadded}</span>${element.name}</div>
+                <i class="icon fa-solid fa-chevron-down"></i>
+              </button>
+              <div class="panel">
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+                  velit esse pariatur, accusamus eos consequuntur ex voluptatibus
+                  voluptatem adipisci officiis labore accusantium ut exercitationem,
+                  at laboriosam minus, sint perferendis repudiandae architecto alias
+                  impedit doloribus. Quasi impedit explicabo molestiae cupiditate
+                  ratione? Ex voluptatibus repellat voluptates velit inventore
+                  beatae non eaque officia nulla asperiores, perferendis saepe
+                  obcaecati omnis!
+                </p>
+              </div>
+        </div>
+      `;
+      accordion.insertAdjacentHTML("afterbegin", html);
+    });
+
+  // After inserting, attach event listeners to the accordion buttons
+  const acc = document.getElementsByClassName("accordion-btn");
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      const panel = this.nextElementSibling;
+      const icon = this.querySelector(".icon");
+
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+}
